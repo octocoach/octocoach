fb535cf3bae7bc1434b1e38ec4d4031fd0d85dd2
Adriaan van der Bergh <avanderbergh@gmail.com>
2023-06-22 19:19:44 +0200

    export lightcast functions Fixes #20

--- packages/job-scraper/src/skills.ts
+++ packages/job-scraper/src/skills.ts
@@ -3,26 +3,52 @@ import got from "got";
 const client_id = process.env.LIGHTCAST_CLIENT_ID;
 const client_secret = process.env.LIGHTCAST_SECRET;
 
-const { access_token } = (await got
-  .post("https://auth.emsicloud.com/connect/token", {
-    form: {
-      client_id,
-      client_secret,
-      grant_type: "client_credentials",
-      scope: "emsi_open",
-    },
-  })
-  .json()) as { access_token: string };
+export const getAccessToken = async (): Promise<string> => {
+  const { access_token } = (await got
+    .post("https://auth.emsicloud.com/connect/token", {
+      form: {
+        client_id,
+        client_secret,
+        grant_type: "client_credentials",
+        scope: "emsi_open",
+      },
+    })
+    .json()) as { access_token: string };
 
-const searchParams = new URLSearchParams({ q: "PHP" });
+  return access_token;
+};
 
-const response = (await got
-  .get("https://emsiservices.com/skills/versions/latest/skills", {
-    headers: {
-      Authorization: `Bearer ${access_token}`,
-    },
-    searchParams,
-  })
-  .json()) as { data: any[] };
+export const getSkills = async (q: string, access_token: string) => {
+  const searchParams = new URLSearchParams({
+    q,
+    fields: "id,name,type,tags,isSoftware,isLanguage,category,subcategory",
+  });
 
-console.log(response.data);
+  const response = (await got
+    .get("https://emsiservices.com/skills/versions/latest/skills", {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+      searchParams,
+    })
+    .json()) as { data: any[] };
+
+  return response.data;
+};
+
+export const extractSkills = async (text: string, access_token) => {
+  const response = (await got
+    .post("https://emsiservices.com/skills/versions/latest/extract", {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text,
+        confidenceThreshold: 0.6,
+      }),
+    })
+    .json()) as { data: any[] };
+
+  return response.data;
+};

32009bffde8d13b63c3bfd04ccef9f89bc40add8
Adriaan van der Bergh <avanderbergh@gmail.com>
2023-09-20 10:52:46 +0200

    Update auth-drizzle adapter

--- packages/auth-drizzle-adapter/schema/columns.ts
+++
@@ -1,9 +0,0 @@
-import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
-
-export const userColumns = {
-  id: text("id").primaryKey(),
-  name: text("name"),
-  email: text("email").notNull(),
-  emailVerified: timestamp("email_verified", { mode: "date" }),
-  image: text("image"),
-};
--- packages/auth-drizzle-adapter/schema/schema.ts
+++
@@ -1,6 +0,0 @@
-import { pgTable } from "drizzle-orm/pg-core";
-import { userColumns } from "./columns";
-
-const slug = process.env.SLUG;
-
-export const users = pgTable("user", userColumns);

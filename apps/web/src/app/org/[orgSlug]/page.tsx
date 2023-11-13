713b4822a4c2e5ea41fa43a12806e8e9ce21bb88
Adriaan van der Bergh <avanderbergh@gmail.com>
2023-11-13 12:48:49 +0100

    Add FAQ Section to UI library Delete unused components

--- apps/web/src/app/org/[orgSlug]/page.tsx
+++ apps/web/src/app/org/[orgSlug]/page.tsx
@@ -1,66 +1,30 @@
 "use client";
 
-import { Faq } from "@components/Faq";
 import { useSession } from "@octocoach/auth/react";
 import {
   AboutSection,
   AboutSectionContent,
-  Box,
   CoachSection,
   CoachSectionContent,
+  FAQSection,
+  FaqSectionContent,
   HeroSection,
   HeroSectionContent,
   MethodSection,
   MethodSectionContent,
   PixelBackground,
-  Stack,
-  Text,
   aboutSectionId,
   coachSectionId,
+  faqSectionId,
   heroSectionId,
   methodSectionId,
 } from "@octocoach/ui";
 import { getContentById } from "@octocoach/ui/helpers";
-import { StaticImageData } from "next/image";
-import { useState } from "react";
-import discover from "./_images/discover.png";
-import grow from "./_images/grow.png";
-import thrive from "./_images/thrive.png";
 import { useOrganization } from "./context";
-
-const Section = ({
-  title,
-  description,
-  src,
-}: {
-  title: string;
-  description: string;
-  src: StaticImageData;
-}) => (
-  <Stack align="center" spacing="tight">
-    <img
-      src={src.src}
-      height={200}
-      width={200}
-      alt={description}
-      style={{ imageRendering: "pixelated" }}
-    />
-
-    <Text element="h2" size="xl" variation="casual" weight="semiBold">
-      {title}
-    </Text>
-
-    <Text width={200} textAlign="center">
-      {description}
-    </Text>
-  </Stack>
-);
 
 export default function Page({ params }: { params: { orgSlug } }) {
   const { data: session } = useSession();
   const organization = useOrganization();
-
-  const [openQA, setOpenQA] = useState<number | null>(null);
 
   const heroSection = getContentById<HeroSectionContent>(
     organization.content,
@@ -82,6 +46,11 @@ export default function Page({ params }: { params: { orgSlug } }) {
     methodSectionId
   );
 
+  const faqSection = getContentById<FaqSectionContent>(
+    organization.content,
+    faqSectionId
+  );
+
   return (
     <>
       <PixelBackground>
@@ -100,28 +69,9 @@ export default function Page({ params }: { params: { orgSlug } }) {
       <PixelBackground pixelSize={80}>
         <CoachSection content={coachSection} />
       </PixelBackground>
-      <Box paddingX="none">
-        <Text element="h2" size="l" weight="bold">
-          FAQs
-        </Text>
-        <Stack>
-          {[].map((qa, idx) => (
-            <Faq
-              key={qa.id}
-              qa={qa}
-              idx={idx}
-              onOpen={(i) => {
-                if (openQA === idx) {
-                  setOpenQA(null);
-                } else {
-                  setOpenQA(i);
-                }
-              }}
-              isOpen={idx === openQA}
-            />
-          ))}
-        </Stack>
-      </Box>
+      <PixelBackground pixelSize={120}>
+        <FAQSection content={faqSection} />
+      </PixelBackground>
     </>
   );
 }

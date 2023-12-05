252800b1faf6c2940b9e8206831245907931c630
Adriaan van der Bergh <avanderbergh@gmail.com>
2023-12-05 22:25:30 +0100

    Fix '

--- apps/web/src/app/org/[orgSlug]/(app)/start/page.tsx
+++ apps/web/src/app/org/[orgSlug]/(app)/start/page.tsx
@@ -23,11 +23,11 @@ export default async function Page({
         />
         <Stack>
           <>
-            <Text size="l">Hi, I'm Adriaan!</Text>
+            <Text size="l">Hi, I&apos;m Adriaan!</Text>
             <Text>
-              Let's begin a quick journey to discover which tech tasks excite
-              you. Just share what you're drawn to—no right or wrong answers
-              here. Next, we'll casually gauge your skills. Think of it as a
+              Let&apos;s begin a quick journey to discover which tech tasks excite
+              you. Just share what you&apos;re drawn to—no right or wrong answers
+              here. Next, we&apos;ll casually gauge your skills. Think of it as a
               self-reflection on your experience, from beginner steps to expert
               strides. Your open and honest insights will help us identify the
               jobs that suit you and the skills you might want to develop.
@@ -39,7 +39,7 @@ export default async function Page({
           <Stack direction="horizontal" justify="right">
             <ButtonLink
               href={`${baseUrl}discover`}
-              text="Let's Go"
+              text="Let&apos;s Go"
               Element={Link}
             />
           </Stack>

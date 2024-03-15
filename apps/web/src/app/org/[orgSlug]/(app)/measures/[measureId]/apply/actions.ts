b45f113bafa6266db65e7c9be725297babd079f6
Adriaan van der Bergh <avanderbergh@gmail.com>
2024-03-15 11:16:21 +0100

    👷Email notifications for enrollment applicationsFixes #146
    * 👷Email notifications for enrollment applications
    Fixes #146

--- apps/web/src/app/org/[orgSlug]/(app)/measures/[measureId]/apply/actions.ts
+++ apps/web/src/app/org/[orgSlug]/(app)/measures/[measureId]/apply/actions.ts
@@ -2,11 +2,11 @@
 
 import { authOrRedirect } from "@helpers/auth";
 import { orgDb } from "@octocoach/db/connection";
-import {
-  NewEnrollment,
-  mkEnrollmentTable,
-} from "@octocoach/db/schemas/org/enrollment";
+import { eq } from "@octocoach/db/operators";
+import { NewEnrollment } from "@octocoach/db/schemas/org/enrollment";
+import { mkOrgSchema } from "@octocoach/db/schemas/org/schema";
 import { revalidatePath } from "next/cache";
+import { Resend } from "resend";
 
 export type CreateEnrollmentParams = Pick<
   NewEnrollment,
@@ -20,9 +20,42 @@ export const createEnrollment = async (
   const db = orgDb(orgSlug);
 
   const { user } = await authOrRedirect(orgSlug);
-  const enrollmentTable = mkEnrollmentTable(orgSlug);
+  const { enrollmentTable, measureTable, userTable } = mkOrgSchema(orgSlug);
+
+  const ownerEmail = await db
+    .select({ email: userTable.email })
+    .from(measureTable)
+    .innerJoin(userTable, eq(userTable.id, measureTable.owner))
+    .where(eq(measureTable.id, enrollment.measure))
+    .then((rows) => rows[0]?.email ?? null);
 
   await db.insert(enrollmentTable).values({ ...enrollment, coachee: user.id });
+
+  if (ownerEmail && user.email) {
+    const key = process.env.RESEND_KEY;
+    if (key) {
+      const resend = new Resend(key);
+      try {
+        const { error } = await resend.emails.send({
+          from: "OctoCoach <no-reply@notifications.octo.coach>",
+          to: [ownerEmail],
+          subject: "New Enrollment",
+          text: `The user ${user.name} has applied for measure ${
+            enrollment.measure
+          }
+
+          ${JSON.stringify(enrollment.screeningAnswers?.questions)}
+          `,
+        });
+
+        if (error) console.error(error);
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      console.warn("No resend API Key!");
+    }
+  }
 
   revalidatePath("/org/[orgSlug]/(app)/measures/[measureId]/apply", "page");
 };

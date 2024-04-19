deff46df9c75e200274a0a582ce38314e71254f0
Adriaan van der Bergh <avanderbergh@gmail.com>
2024-04-19 09:46:13 +0000

    External Links in Markdown should open in new tab Fixes #180

--- packages/ui/Markdown/Markdown.tsx
+++ packages/ui/Markdown/Markdown.tsx
@@ -1,3 +1,4 @@
+import { Launch } from "@carbon/icons-react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
@@ -5,7 +6,25 @@ import { markdown } from "./markdown.css";
 
 export const Markdown = ({ children }: { children: string }) => {
   return (
-    <ReactMarkdown className={markdown} remarkPlugins={[remarkGfm]}>
+    <ReactMarkdown
+      className={markdown}
+      remarkPlugins={[remarkGfm]}
+      components={{
+        a: ({ href, children }) => {
+          if (!href) {
+            throw new Error(`Missing href for Markdown Link`);
+          }
+          return (
+            <a href={href} target="_blank" rel="noreferrer">
+              {children}
+              {href.startsWith("http") && (
+                <Launch style={{ fontSize: "1em", display: "inline" }} />
+              )}
+            </a>
+          );
+        },
+      }}
+    >
       {children}
     </ReactMarkdown>
   );

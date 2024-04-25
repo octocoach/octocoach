c5cfa686d56fe17c7daf8726db0e4e06c7ed0aa9
Adriaan van der Bergh <avanderbergh@gmail.com>
2024-04-25 20:18:49 +0000

    👷 Meeting discussion summaries Fixes #188

--- packages/daily/transcripts.ts
+++ packages/daily/transcripts.ts
@@ -1,17 +1,20 @@
 import { Daily } from ".";
+import { WebVTT } from "./webvtt";
 
 const daily = new Daily();
 
-const transcripts = await daily.listTranscripts({ roomName: "kdz-uov-axf" });
+const transcripts = await daily.listTranscripts({ roomName: "gfa-amq-rlg" });
+
+console.log(transcripts);
 
 for (const transcript of transcripts) {
   const link = await daily.getTranscriptLink(transcript.transcriptId);
-
-  console.log(link);
 
   const res = await fetch(link);
 
   const text = await res.text();
 
-  console.log(text);
+  const webVTT = new WebVTT(text);
+
+  console.log(webVTT.clean());
 }

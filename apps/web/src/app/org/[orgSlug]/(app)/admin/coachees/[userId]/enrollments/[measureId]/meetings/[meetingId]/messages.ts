f6194ded4d92285c80b304a9a21184a001b0cab7
Adriaan van der Bergh <avanderbergh@gmail.com>
2024-05-02 07:04:23 +0000

    👷 Meeting discussion summaries Fixes #188

--- apps/web/src/app/org/[orgSlug]/(app)/admin/coachees/[userId]/enrollments/[measureId]/meetings/[meetingId]/messages.ts
+++ apps/web/src/app/org/[orgSlug]/(app)/admin/coachees/[userId]/enrollments/[measureId]/meetings/[meetingId]/messages.ts
@@ -9,6 +9,9 @@ export const transcriptSystemMessage = [
 
 export const summarySystemMessage = [
   "You will be provided with a transcribed conversation between a coach and a coachee.",
+  "The transcription might contain some misheard words or other mistakes.",
+  "Use the overall context of the conversation and try to fix the transcription errors.",
+  "Use the context of the discussion to replace words that seem out of place with words that would make sense in that context",
   "Your task is to summarize the the main points of the discussion in a follow-up message from the coach to the coachee.",
   "The summary should be formatted in Markdown, using bullet points and headings to make it easily readable.",
   "Use the speaking style of the coach to inform how you write the summary.",
--- apps/web/src/app/org/[orgSlug]/(app)/admin/coachees/[userId]/enrollments/[measureId]/meetings/[meetingId]/transcript.tsx
+++ apps/web/src/app/org/[orgSlug]/(app)/admin/coachees/[userId]/enrollments/[measureId]/meetings/[meetingId]/transcript.tsx
@@ -1,12 +1,5 @@
 import { Daily } from "@octocoach/daily";
-import { Card } from "@octocoach/ui/Card/Card";
-import { Markdown } from "@octocoach/ui/Markdown/Markdown";
-import { Stack } from "@octocoach/ui/Stack/Stack";
-import { Text } from "@octocoach/ui/Text/Text";
-import { render } from "ai/rsc";
-import OpenAI from "openai";
 
-import { transcriptSystemMessage } from "./messages";
 import { Summary } from "./summary";
 
 export const Transcript = async ({ id }: { id: string }) => {
@@ -14,39 +7,5 @@ export const Transcript = async ({ id }: { id: string }) => {
 
   const transcript = await daily.getTranscriptContent(id);
 
-  const openai = new OpenAI();
-
-  const fixed = render({
-    model: "gpt-4-turbo",
-    provider: openai,
-    messages: [
-      {
-        role: "system",
-        content: transcriptSystemMessage,
-      },
-      { role: "user", content: transcript },
-    ],
-    text: ({ content, done }) => {
-      return (
-        <Stack>
-          <Card>
-            <Text size="l" weight="bold">
-              Conversation
-            </Text>
-            <Markdown>{content}</Markdown>
-          </Card>
-          {done && (
-            <Card>
-              <Text size="l" weight="bold">
-                Summary
-              </Text>
-              <Summary transcript={content} />
-            </Card>
-          )}
-        </Stack>
-      );
-    },
-  });
-
-  return <>{fixed}</>;
+  return <Summary transcript={transcript} />;
 };

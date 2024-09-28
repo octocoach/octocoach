b1901cb837554925ed17aa72b31525397cc8af86
Adriaan van der Bergh <avanderbergh@gmail.com>
2024-09-28 12:42:26 +0000

    Add no-wrap to external link icon

--- packages/ui/Markdown/Markdown.tsx
+++ packages/ui/Markdown/Markdown.tsx
@@ -15,7 +15,12 @@ export const Markdown = ({ children }: { children: string }) => {
             throw new Error(`Missing href for Markdown Link`);
           }
           return (
-            <a href={href} target="_blank" rel="noreferrer">
+            <a
+              href={href}
+              target="_blank"
+              rel="noreferrer"
+              style={{ textWrap: "nowrap" }}
+            >
               {children}
               {href.startsWith("http") && (
                 <Launch style={{ fontSize: "1em", display: "inline" }} />
